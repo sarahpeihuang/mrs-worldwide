6c52863cfcbd6bcb55f37b49cd878f4b606ee94a
Claire <czhan828@uwo.ca>
2024-01-13 14:52:30 -0500

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import pages and components
 import Home from './pages/home'
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route 
