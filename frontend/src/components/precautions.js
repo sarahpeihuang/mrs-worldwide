d21531f6fc1c933cc9bae36d307f92c9f23b648c
sarah888 <shuan392@uwo.ca>
2024-01-13 16:03:32 -0500

    added tailwind css and finished the housing component! need to replace w actual variable

---
+++ frontend/src/components/precautions.js
@@ -0,0 +1,9 @@
+const Precautions = () => {
+    return(
+        <div className="precautions">
+            <h2>Precautions</h2>
+        </div>
+    )
+}
+
+export default Precautions
---
+++ frontend/src/components/safe_cities.js
@@ -0,0 +1,11 @@
+const Safe = () => {
+    return(
+        <header>
+        <div className="safe">
+            <h2>Safe Cities you can consider</h2>
+        </div>
+        </header>
+    )
+}
+
+export default Safe
