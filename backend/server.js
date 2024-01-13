c1a34ddb7913056296c77f9b5637c19c775985a0
Claire <czhan828@uwo.ca>
2024-01-13 10:57:38 -0500

    route setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const itineraryRoutes = require('./routes/itinerary')
 
 // express app
 const app = express()
 
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'welcome to mrs worldwide'})
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
 })
+
+// routes 
+app.use('/api/itinerary', itineraryRoutes)
 
 // listening for requests
 app.listen(process.env.PORT, () => {
