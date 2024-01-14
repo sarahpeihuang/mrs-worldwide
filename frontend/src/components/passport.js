7198a9f4514f1d3533b6dc12d0dd69961e10f76d
Claire <czhan828@uwo.ca>
2024-01-14 03:17:53 -0500

    womp

--- frontend/src/components/passport.js
+++ frontend/src/components/passport.js
@@ -5,16 +5,7 @@ const Passport = () => {
             <h2 className="text-[#e933a3] font-bold text-2xl mb-4">
                 Passport Validity: # of VISA free days in Paris
             </h2>
-            <p style={{ fontSize: '1.2em' }}>
-                <strong>Montpellier</strong> 55.16 44.84<br />
-                <strong>Lille</strong> 54.33 45.67<br />
-                <strong>Grenoble</strong> 54.2 45.8<br />
-                <strong>Rennes</strong> 53 47<br />
-                <strong>Lyon</strong> 51.29 48.71<br />
-                <strong>Toulouse</strong> 48.92 51.08<br />
-                <strong>Bordeaux</strong> 48.39 51.61<br />
-                <strong>Strasbourg</strong> 37.93 62.07<br />
-            </p>
+            <p><strong>90 days</strong></p>
             </div>
         </header>
     )
--- frontend/src/components/safe_cities.js
+++ frontend/src/components/safe_cities.js
@@ -16,6 +16,20 @@ const Safe = () => {
                 <p>2. Learn some basic French phrases to help you navigate and communicate with locals. Knowing how to communicate basic information can make you less of a target for tourists.</p>
                 <br></br>
                 <p>3. Make an emergency plan. Share your travel plans with trusted family or friends at home, carry a copy of your passport at all times, and have the contact information for the local embassy or consulate easily accessible.</p>
+                <br></br>
+                <h1 className="text-[#e933a3] font-bold text-2xl mb-4">French Cities Safer than Paris</h1>
+                <h3 className="text" style={{ fontStyle: 'italic' }}>City, Crime Index, Safety Index</h3>
+                <br></br>
+                <p style={{ fontSize: '1.2em' }}>
+                <strong>Montpellier</strong> 55.16 44.84<br />
+                <strong>Lille</strong> 54.33 45.67<br />
+                <strong>Grenoble</strong> 54.2 45.8<br />
+                <strong>Rennes</strong> 53 47<br />
+                <strong>Lyon</strong> 51.29 48.71<br />
+                <strong>Toulouse</strong> 48.92 51.08<br />
+                <strong>Bordeaux</strong> 48.39 51.61<br />
+                <strong>Strasbourg</strong> 37.93 62.07<br />
+            </p>
             </div>
         </header>
     );
